130284a4c323484c23fe8eea92e127b1299684aa
JJablonski <101069566+JJablonski02@users.noreply.github.com>
2023-06-08 18:31:43 +0200

    ModelValidations

---
+++ ModelValidations/ModelValidations/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidations.Models;
+namespace ModelValidations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        //public IActionResult Index([Bind(nameof(Person.PersonName), nameof(Person.Email), 
+        //    nameof(Person.Password), nameof(Person.ConfirmPassword))]Person person)
+        public IActionResult Index([FromBody]Person person) 
+         {
+            List<string> errorsList = new List<string>();
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("\n",
+                    ModelState.Values.SelectMany(value => value.Errors).Select
+                    (err => err.ErrorMessage));
+               return BadRequest(errors);
+            }
+            return Content($"{person}");
+        }
+    }
+}
---
+++ ModelValidations/ModelValidations/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidations.CustomValidators
+{
+    public class MinimumYearValidatorAttribute : ValidationAttribute
+    {
+        public int MinimumYear { get; set; } = 2000;
+        public string DefaultErrorMessage { get; set; } = "Year should be less than {0}";
+        public MinimumYearValidatorAttribute()
+        {
+        }
+        public MinimumYearValidatorAttribute(int minimumYear)
+        {
+            MinimumYear = minimumYear;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime date = (DateTime)value;
+                if (date.Year >= MinimumYear)
+                {
+                    return new ValidationResult(string.Format
+                        (ErrorMessage ?? DefaultErrorMessage, MinimumYear)); // ?? if erromessage is null it returns defaulterrormessage
+                }
+                else
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return null;
+        }
+    }
+}
