a32983badaba533e7f8975cd08b73fce28a92c0c
JJablonski <101069566+JJablonski02@users.noreply.github.com>
2023-06-12 01:26:41 +0200

    WeatherApp

---
+++ WeatherApp/WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        [Route("/")]
+        public IActionResult Weather()
+        {
+            List<CityWeather> cityWeathers = new List<CityWeather>()
+            {
+                new CityWeather{CityUniqueCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2023-06-12 00:55")},
+                new CityWeather{CityUniqueCode = "NYC", CityName = "New York", DateAndTime = DateTime.Parse("2023-06-11 19:55")},
+                new CityWeather{CityUniqueCode = "PRS", CityName = "Paris", DateAndTime = DateTime.Parse("2023-06-12 01:55")},
+            };
+
+            ViewBag.cityWeathers = cityWeathers;
+            return View(cityWeathers);
+        }
+    }
+}
