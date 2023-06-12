4607bbb86174b9e3e0a2b433ba248d2da7b4d82c
JJablonski <101069566+JJablonski02@users.noreply.github.com>
2023-06-12 15:46:14 +0200

    PartialViews

---
+++ PartialViews/PartialViews/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViews.Models;
+
+namespace PartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["ListTitle"] = "Cities";
+            ViewData["ListItems"] = new List<string>()
+            {
+                "Paris",
+                "New York",
+                "New Mumbai",
+                "Rome"
+            };
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguage()
+        {
+            ListModel listmodel = new ListModel()
+            {
+                ListTitle = "Programming Language List",
+                ListItems = new List<string>()
+                {
+                    "Python",
+                    "C#",
+                    "Go",
+                    "JS",
+                    "React"
+                }
+            };
+            return PartialView("_LKistPartialView", listmodel);
+        }
+    }
+}
